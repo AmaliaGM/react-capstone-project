aae058935d58a1380f297b3821edd57f23687451
amalia gomez moro <mariaamaliagomezmoro@gmail.com>
2022-09-22 16:13:40 -0300

    styling nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,7 +4,7 @@ import img from './images/monet.jpg';
 function Nav() {
   return (
     <header style={{ display: 'flex' }}>
-      <img className="headerImg" src={img} alt="Monet" width={100} />
+      <img className="headerImg" src={img} alt="Monet" width={150} />
       <div>
         <h1 className="headerText">Monet Art Work</h1>
       </div>
